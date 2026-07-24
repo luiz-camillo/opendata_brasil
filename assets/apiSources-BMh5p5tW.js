f8610e9ea1b15b9f33196d1564d2aab7c5702db7
camillo <lazarettieb@gmail.com>
2026-07-24 12:31:41 -0300

    Updates

---
+++ assets/apiSources-BMh5p5tW.js
@@ -0,0 +1,1 @@
+var e=[{id:`ibge`,nome:`IBGE - Instituto Brasileiro de Geografia e Estatística`,descricao:`Dados estatísticos, geográficos e demográficos do Brasil`,orgao:`IBGE`,url:`https://servicodados.ibge.gov.br/api`,documentacao:`https://servicodados.ibge.gov.br/api/docs`,frequencia:`Variável por pesquisa`,formato:`JSON`,cors:!0,ativa:!0}];export{e as t};
